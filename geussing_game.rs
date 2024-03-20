ae8966eef73f2f2c276d587f2d5aedf77265d41b
Roddy <roddymcg787@gmail.com>
2024-03-20 16:32:13 -0700

    i a little dum

---
+++ geussing_game.rs
@@ -0,0 +1,8 @@
+main() {
+    const winning_geuss: u32 = 23
+    println!("geuss the number")
+    // not gonna lie i have no idea to handle io in rust
+    // c based launages are hard
+    // this is a comment
+    // im just rambling
+}
